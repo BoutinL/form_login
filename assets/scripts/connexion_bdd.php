bbe2d10d14791724f60465bfd7a86e9675c5fbe4
unknown <boutin.loic@hotmail.com>
2022-07-05 11:27:41 +0200

    connexion working

--- assets/scripts/connexion_bdd.php
+++ assets/scripts/connexion_bdd.php
@@ -1,5 +1,24 @@
 <?php
-if (isset($_POST['submit'])) {
-    require("./assets/scripts/link_bdd.php");
-}
+    if (!empty($_POST['submit'])) {
+        require_once("assets/scripts/link_bdd.php");
+        
+        $login = htmlspecialchars($_POST["username"]); 
+        $pass = htmlspecialchars($_POST["pass"]);
+
+        $res = $pdo -> prepare("SELECT * FROM users WHERE user_login = ?");
+        $res -> setFetchMode(PDO::FETCH_ASSOC);
+        $res -> execute(array($login));
+        $tab = $res -> fetchAll();
+
+        if(empty($tab)) {
+            echo "L'utilisateur n'existe pas.";
+        } else {
+            if (password_verify($pass, $tab[0]["user_password"])) {
+                echo "L'utilisateur existe et le mot de passe est OK.";
+                header('location:./espacePrive.php');
+            } else {
+                echo "L'utilisateur existe mais mot de passe est incorrect !";
+            }
+        }
+    }
 ?>
